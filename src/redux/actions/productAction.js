47fd135498b2162738720910513dc1416a429050
Hwiho Lee <lhh323@gmail.com>
2024-04-03 21:09:42 +0900

    4/3

---
+++ src/redux/actions/productAction.js
@@ -0,0 +1,21 @@
+// import {productActions} from '../reducers/productSlice'
+
+// function getProducts(searchQuery){
+//   return async(dispatch, getState)=>{
+//     let url = `https://my-json-server.typicode.com/wammaa/hnmreact/products?q=${searchQuery}`
+//     let response = await fetch(url)
+//     let data = await response.json()
+//     // dispatch({type:"GET_PRODUCT_SUCCESS", payload:{data}})
+//     dispatch(productActions.getAllProducts({data}))
+//   }
+// }
+
+// function getProductDetail(id){
+//   return async(dispatch)=>{
+//     let url = `https://my-json-server.typicode.com/wammaa/hnmreact/products/${id}`
+//     let response = await fetch(url)
+//     let data = await response.json()
+//     dispatch({ type: "GET_PRODUCT_DETAIL_SUCCESS", payload: { data } });
+//   }
+// }
+// export const productAction={getProductDetail}
